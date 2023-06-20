89bfd442d612fc963b047abfbcb9efca9100fdc3
Angela-coello <angeladarko32@gmail.com>
2023-06-20 20:12:07 +0200

    dad jokes terminada

---
+++ script.js
@@ -0,0 +1,23 @@
+const jokeElement = document.getElementById('joke');
+const jokeButton = document.getElementById ('jokebtn');
+
+jokeButton.addEventListener('click', generateJoke);
+
+
+
+async function generateJoke() {
+    const config = {
+        headers: {
+            Accept: 'application/json',
+        }
+    }
+
+ 
+const resp = await fetch('https://icanhazdadjoke.com', config);
+const data = await resp.json();
+
+
+jokeElement.innerHTML = data.joke;
+
+}
+
